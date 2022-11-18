6e4d2c905c3f0e59a2fff54b4f7a0cc9116d59b6
pritalb <work.prital@gmail.com>
2022-11-18 17:55:33 +0530

    reciprocal_cycles.py: done

--- reciprocal_cycles.py
+++ reciprocal_cycles.py
@@ -19,40 +19,20 @@ Created on 9 Nov 2022 at 5:29 pm
 
 # Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
 
-def get_recurring_fractions(n):
-    '''
-    In:
-        n: int
 
-    Out:
-        a list of tuples of n and 1/n. only tuples with 1/n being a recurring fraction are considered.
-    '''
-    out = []
-    recurring_fract_len = len(str(1/3))
-
-    for i in range(2, n):
-        fract = 1/i
-        if len(str(fract)) >= recurring_fract_len:
-            out.append((i, fract))
-
-    return out
-
-def test_get_recurring_fractions():
-    n = 10
-    res = get_recurring_fractions(n)
-    print(*res, sep='\n')
+from decimal import getcontext, Decimal
 
 # returns a list of slices of str. the slices will be of the specified size.
-def split_into_parts(inStr, size):
+def split_into_parts(inputStr, size):
     out = []
-    str_len = len(inStr)
+    str_len = len(inputStr)
     start_index = 0
     end_index = 0
 
     while end_index <= str_len:
         end_index = start_index + size
 
-        out.append(inStr[start_index : end_index])
+        out.append(inputStr[start_index : end_index])
         start_index = end_index
 
     return out
@@ -60,23 +40,25 @@ def split_into_parts(inStr, size):
 # check if all elements in a list are identical. all elements must be str or lists
 def are_same(ls):
     if ls == [] or len(ls) == 1:
-        return True
+        return False
     
     first_elem = ls[0]
     last_elem = ls[-1]
 
-    for elem in ls[:-1]:
+    for elem in ls[1:-1]:
         if elem != first_elem:
             return False
     
-    if len(first_elem) > len(last_elem):
+    if (len(first_elem) > len(last_elem)) and (len(last_elem) > 10):
         return first_elem[:len(last_elem)] == last_elem
     else:
         return first_elem == last_elem
 
 def find_recurring_part(fract):
-    for size in range(1, len(fract)):
-        parts = split_into_parts(fract, size)
+    temp = fract[:]
+
+    for size in range(1, len(temp)):
+        parts = split_into_parts(temp, size)
         found = are_same(parts)
 
         if found:
@@ -114,24 +96,85 @@ def format_fract(fract):
     fract_parts = get_recurring_part(fract)
     return f'{fract_parts[0]}({fract_parts[1]})'
 
+def test_find_recurring_part():
+    n = 659
+    # n = 6
+    fract = str(Decimal(1) / Decimal(n))[:-1]
+    print(find_recurring_part(fract))
+
 def test_get_recurring_part():
-    n = 3
-    print(get_recurring_part(1/n))
+    n = 659
+    # n = 6
+    fract = str(Decimal(1) / Decimal(n))[:-1]
+
+    print(fract)
+    print(get_recurring_part(fract))
 
 def test_split_into_parts():
     size = 4
     s = 'abcdefghij'
     print(split_into_parts(s, size))
 
-def test_get_recurring_part():
-    s = '123456'
-    print(get_recurring_part(s))
-
 def test_format_fraction():
-    fract = '0.67121212121212'
-    print(format_fact(fract))
+    fract = str(Decimal(1) / Decimal(97))
+
+    # the last digit of the fraction is rounded up, so it needs to be gotten rid of
+    fract = fract[:-1]
+    
+    print(fract)
+    print(format_fract(fract))
+
+
+
+def get_recurring_fractions(n):
+    '''
+    In:
+        n: int
+
+    Out:
+        a list of tuples of n and 1/n. only tuples with 1/n being a recurring fraction are considered.
+    '''
+    out = {}
+
+    recurring_fract_len = len(str(Decimal(1) / Decimal(3))[:-1]) - 10 #10 is error correction since the fraction will also contain stuff like '0.'
+
+    for i in range(2, n):
+        fract = str(Decimal(1) / Decimal(i))[2:-1] # the '0.' at the start is not needed
+        if len(fract) >= recurring_fract_len:
+            temp = get_recurring_part(fract)
+
+            recurring_part_size = len(temp[1])
+            i_out = (i, temp[0], temp[1])
+
+            if recurring_part_size in out.keys():
+                out[recurring_part_size].append(i_out)
+            else:
+                out[recurring_part_size] = [i_out]
+    return out
+
+def test_get_recurring_fractions():
+    n = 50
+    res = get_recurring_fractions(n)
+    print(res)
+
+
 
 if __name__ == '__main__':
-    fract = '0.13333333333'
+    #setting precision of decimal
+    getcontext().prec = 1000
 
-    print(format_fract(fract))
+    n = 1000
+
+    fracts = get_recurring_fractions(n)
+    # print(fracts)
+
+    biggest_key = max(fracts.keys())
+    print(biggest_key)
+    
+    num_with_biggest_key = fracts[biggest_key]
+    print(num_with_biggest_key)
+
+    # test_format_fraction()
+    # test_get_recurring_fractions(100)
+    # test_find_recurring_part()
+    # test_get_recurring_part()
